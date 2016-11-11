ec03592f6b0915806f1454303381583d48aa8fc7
guicaiol <gcaixetaoliveira@gmail.com>
2016-11-11 20:55:16 -0200

    added project/license header to all files

--- src/UVF-GA/simulation/player/navigation/controlalgorithm/pid/pid.cc
+++ src/UVF-GA/simulation/player/navigation/controlalgorithm/pid/pid.cc
@@ -1,3 +1,30 @@
+/***
+ * SSC0713 - Sistemas Evolutivos Aplicados à Robótica
+ * University of Sao Paulo (USP) at Sao Carlos
+ *
+ * Autores:
+ * Guilherme Caixeta de Oliveira
+ * Anderson Hiroshi de Siqueira
+ * Leonardo Claudio de Paula e Silva
+ * Lucas dos Santos Luchiari
+ * Franco Saraiva
+ *
+ * This file is part of UVF-GA project.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "pid.hh"
 #include <iostream>
 
