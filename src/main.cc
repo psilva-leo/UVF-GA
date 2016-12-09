09c0137d276f9eaefedc26fc5cb16d955f6a2806
guicaiol <gcaixetaoliveira@gmail.com>
2016-12-09 02:03:46 -0200

    implemented save/load last better result for visualization; two modes: 0-GA, 1-view

--- src/main.cc
+++ src/main.cc
@@ -26,6 +26,7 @@
  */
 
 #include <iostream>
+#include <fstream>
 #include <QApplication>
 #include <UVF-GA/geneticalgorithm/population.hh>
 #include <3rdparty/soccerview/soccerview.hh>
@@ -35,76 +36,16 @@
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-#define POPULATION_SIZE 10
-#define MUTATION_RATE 0.6
-#define CROSSOVER_RATE 0.6
-#define MAX_ITERATIONS 2
+#define POPULATION_SIZE 5
+#define MUTATION_RATE 0.75
+#define CROSSOVER_RATE 0.5
+#define MAX_ITERATIONS 50
 #define OLD_POP_SLECTION_RATE 0.05
 
 #define VIEW_STEP (1/60.0f) // seconds
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-
-    srand(time(NULL));
-    int iteration = 1;
-
-    time_t start;
-    time(&start);
-
-    // Run GA
-    cout << "Generating initial random population...\n";
-    Population *pop = new Population(POPULATION_SIZE);
-
-    cout << "Evaluating initial population...\n\n";
-    pop->evaluate();
-
-    QList<Population> popList;
-    while(iteration <= MAX_ITERATIONS) {
-        cout << ">>> ITERATION " << iteration << " <<<" << endl;
-        pop->print();
-        cout << "\n";
-
-        Population crossPop = pop->crossOver(CROSSOVER_RATE);
-        Population mutPop = crossPop.mutation(MUTATION_RATE);
-        popList.clear();
-        popList.append(crossPop);
-        popList.append(mutPop);
-        Population selection_old = pop->selection_old(OLD_POP_SLECTION_RATE*POPULATION_SIZE);
-        popList.append(selection_old);
-        pop = pop->selection(popList);
-
-        iteration++;
-    }
-
-    // Last Population
-    cout << "\n Last Population" << endl;
-    pop->print();
-
-    // Get better result
-    Chromosome *better = pop->getBetter();
-    double de    = better->getGen(0)->getValue();
-    double kr    = better->getGen(1)->getValue();
-    double dmin  = better->getGen(2)->getValue();
-    double delta = better->getGen(3)->getValue();
-    double maxASpeed = better->getGen(4)->getValue();
-    double maxLSpeed = better->getGen(5)->getValue();
-
-//    de = 0.15;
-//    kr = 0.50;
-//    dmin = 0.005;
-//    delta = 0.25;
-//    maxASpeed = 4*PI;
-//    maxLSpeed = 1.5;
-
-    // Print
-    cout << "de: " << de << "\nkr: " << kr << "\ndmin: " << dmin << "\ndelta: " << delta << "\nmaxASpeed: " << maxASpeed << "\nmaxLSpeed: " << maxLSpeed << endl;
-    cout << "Fitness: " << better->getFitness() << endl;
-    time_t end;
-    time(&end);
-    cout << "\n>>Execution time: " << ((double) (end - start))/60 << " min" << endl;
-
-    // Visualize better result
+void runView(QApplication *app, double de, double kr, double dmin, double delta, double maxASpeed, double maxLSpeed) {
+    // Visualize better result on SoccerView
     GLSoccerView soccerView;
     Visualizator *view = new Visualizator(&soccerView);
     view->configMovement(Position(-2.5, -1.3), Utils::toRad(120), Position(2.75, 1.75), Utils::toRad(90), true, true);
@@ -116,15 +57,137 @@ int main(int argc, char *argv[]) {
     view->start();
     soccerView.show();
     while(view->wait(1)==false) {
-        app.processEvents();
+        app->processEvents();
         QThread::msleep(5);
     }
     soccerView.close();
+}
 
-    return 0;
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
 
-    // Stop
-    app.exec();
+    // Check arguments
+    if(argc < 3) {
+        std::cout << "Usage: ./UVF-GA <mode> <better-file>\n\n";
+        return EXIT_FAILURE;
+    }
+
+    // Parse arguments
+    const int mode = atoi(argv[1]);
+    const std::string betterFilename = argv[2];
+
+    // Switch mode
+    switch(mode) {
+        default:
+        case 0: { // run GA
+            // Randomization
+            srand(time(NULL));
+
+            // Measure time
+            time_t start;
+            time(&start);
+
+            // Create initial population
+            cout << "Generating initial random population...\n";
+            Population *pop = new Population(POPULATION_SIZE);
+
+            // Evaluate initial population
+            cout << "Evaluating initial population...\n\n";
+            pop->evaluate();
+
+            // Open file to save better
+            std::fstream betterFile(betterFilename, std::ios_base::out | std::ios_base::trunc);
+            if(betterFile.is_open()==false) {
+                std::cout << "Error: failed to open file '" << betterFilename << "'.\n";
+                return EXIT_FAILURE;
+            }
+
+            // Run iterations
+            int iteration = 1;
+            QList<Population> popList;
+            while(iteration <= MAX_ITERATIONS) {
+                cout << ">>> ITERATION " << iteration << " <<<" << endl;
+                pop->print();
+
+                // Clear list
+                popList.clear();
+
+                // Crossover
+                std::cout << "Running crossover...\n";
+                Population crossPop = pop->crossOver(CROSSOVER_RATE);
+                popList.append(crossPop);
+
+                // Mutation
+                std::cout << "Running mutation...\n";
+                Population mutPop = crossPop.mutation(MUTATION_RATE);
+                popList.append(mutPop);
+
+                // Selection
+                std::cout << "Running selection...\n";
+                Population selection_old = pop->selection_old(OLD_POP_SLECTION_RATE*POPULATION_SIZE);
+                popList.append(selection_old);
+                pop = pop->selection(popList);
+
+                // Save better
+                Chromosome *better = pop->getBetter();
+                double de    = better->getGen(0)->getValue();
+                double kr    = better->getGen(1)->getValue();
+                double dmin  = better->getGen(2)->getValue();
+                double delta = better->getGen(3)->getValue();
+                double maxASpeed = better->getGen(4)->getValue();
+                double maxLSpeed = better->getGen(5)->getValue();
+                betterFile.clear();
+                betterFile.seekg(0, std::ios::beg);
+                betterFile << de << " " << kr << " " << dmin << " " << delta << " " << maxASpeed << " " << maxLSpeed << "\n";
+                betterFile.flush();
+
+                // Inc iteration
+                iteration++;
+            }
+
+            // Last Population
+            cout << "\n Last Population" << endl;
+            pop->print();
+
+            // Get better result
+            Chromosome *better = pop->getBetter();
+            double de    = better->getGen(0)->getValue();
+            double kr    = better->getGen(1)->getValue();
+            double dmin  = better->getGen(2)->getValue();
+            double delta = better->getGen(3)->getValue();
+            double maxASpeed = better->getGen(4)->getValue();
+            double maxLSpeed = better->getGen(5)->getValue();
+
+            // Print better and execution time
+            cout << "de: " << de << "\nkr: " << kr << "\ndmin: " << dmin << "\ndelta: " << delta << "\nmaxASpeed: " << maxASpeed << "\nmaxLSpeed: " << maxLSpeed << endl;
+            cout << "Fitness: " << better->getFitness() << endl;
+            time_t end;
+            time(&end);
+            cout << "\n>>Execution time: " << ((double) (end - start))/60 << " min" << endl;
+
+            // Run view
+            runView(&app, de, kr, dmin, delta, maxASpeed, maxLSpeed);
+
+        } break;
+
+        case 1: { // visualize last better
+
+            // Open file
+            std::fstream betterFile(betterFilename, std::ios_base::in);
+            if(betterFile.is_open()==false) {
+                std::cout << "Error: failed to open file '" << betterFilename << "'.\n";
+                return EXIT_FAILURE;
+            }
+
+            // Read from file
+            double de=0, kr=0, dmin=0, delta=0, maxASpeed=0, maxLSpeed=0;
+            betterFile >> kr >> dmin >> delta >> maxASpeed >> maxLSpeed;
+
+            // Run view
+            runView(&app, de, kr, dmin, delta, maxASpeed, maxLSpeed);
+
+        } break;
+    }
 
     return 0;
 }
